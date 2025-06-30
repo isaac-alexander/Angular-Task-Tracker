f0ca4489f22951115633cc4614326f8904fbd59c
Isaac Alexander <Isaacalexander81@gmail.com>
2025-06-30 02:31:31 +0100

    src/app/app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [Header]
 })
 export class App {
-  protected title = 'angular-crash';
 }
