860e9b89216672ec57077b1bbadb0685da830c81
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-01 02:05:07 +0100

    src/app/app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
+import { Tasks } from "./components/tasks/tasks";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Header]
+  imports: [Header, Tasks]
 })
 export class App {
 }
