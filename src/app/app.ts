d638d871a13ef7d3a7ab4f480cbb83690b38a85a
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-03 02:34:02 +0100

    src/app/app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Tasks } from "./components/tasks/tasks";
+import {RouterModule} from '@angular/router';
+import { Footer } from "./components/footer/footer";
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Header, Tasks]
+  imports: [Header, RouterModule, Footer]
 })
 export class App {
 }
