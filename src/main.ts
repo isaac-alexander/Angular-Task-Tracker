4c5bf7472396fffba24547beaf4877e338ad1b85
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-03 02:36:58 +0100

    src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import routeConfig from './app/routes';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    provideRouter(routeConfig),
+    ...(appConfig.providers || [])
+  ]
+}).catch((err) => console.error(err));
