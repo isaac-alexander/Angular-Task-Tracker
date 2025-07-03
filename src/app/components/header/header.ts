1c49922685d9c0328e5ecb81d5e212436b563c9b
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-03 02:35:26 +0100

    src/app/components/header/header.ts

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,22 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Button } from "../button/button";
+import { CommonModule } from '@angular/common';
+import { UiService } from '../../services/ui.service.';
+import { Subscription } from 'rxjs';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-header',
-  imports: [Button],
+  imports: [Button, CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header implements OnInit{
 
-  protected title: string = 'Task Tracker';
+  title: string = 'Task Tracker';
+  showAddTask: boolean = true;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService, private router:Router) {
+    this.subscription = this.uiService
+    .onToogle()
+    .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log('toggle');
-    
+    this.uiService.toogleAddTask(); 
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
