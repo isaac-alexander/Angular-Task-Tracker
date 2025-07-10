3abd0273705fcebbf4abbad730c7b7a31fdecb8b
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-10 18:13:25 +0100

    src/app/app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Header } from "./components/header/header";
-import { Tasks } from "./components/tasks/tasks";
-import {RouterModule} from '@angular/router';
+import {RouterOutlet} from '@angular/router';
 import { Footer } from "./components/footer/footer";
 
 @Component({
@@ -9,7 +8,7 @@ import { Footer } from "./components/footer/footer";
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Header, RouterModule, Footer]
+  imports: [Header, RouterOutlet, Footer]
 })
 export class App {
 }
