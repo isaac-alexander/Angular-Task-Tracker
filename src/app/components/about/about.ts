14e4f7cc28c67dd5f8b0dfa853f5dee02729855b
Isaac Alexander <Isaacalexander81@gmail.com>
2025-07-10 18:13:51 +0100

    src/app/components/about/about.ts

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-about',
-  imports: [CommonModule, RouterModule],
+  imports: [ RouterLink ],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
